152b9ecc750bc083b4ddb5107ddf9045753ad183
Sean Lowen <stonesfan101@hotmail.com>
2014-01-07 22:21:23 -0500

    Added project pages and logo

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,22 @@
+class ProjectsController < ApplicationController
+  def barbot
+  end
+
+  def underactuation
+  end
+
+  def planet_labs
+  end
+
+  def hijack
+  end
+
+  def gibbon
+  end
+
+  def uocd
+  end
+
+  def radar
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Website::Application.routes.draw do
   root 'static_pages#home'
-  match '/projects',  to: 'static_pages#projects',  via: 'get'
   match '/about',     to: 'static_pages#about',     via: 'get'
+  match '/projects',  to: 'static_pages#projects',  via: 'get'
+  match '/projects/barbot',  		to: 'projects#barbot',  		as: 'barbot', 			via: 'get'
+  match '/projects/underactuation',	to: 'projects#underactuation',  as: 'underactuation',	via: 'get'
+  match '/projects/planet_labs',  	to: 'projects#planet_labs',  	as: 'planet_labs',		via: 'get'
+  match '/projects/hijack',  		to: 'projects#hijack',  		as: 'hijack',			via: 'get'
+  match '/projects/gibbon',  		to: 'projects#gibbon',  		as: 'gibbon',			via: 'get'
+  match '/projects/uocd',  			to: 'projects#uocd',  			as: 'uocd',				via: 'get'
+  match '/projects/radar',  		to: 'projects#radar',  			as: 'radar',			via: 'get'
 end
